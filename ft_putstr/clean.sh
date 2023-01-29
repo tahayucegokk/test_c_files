d27c490f6712d414c9f0fab6095c0fb7fdcb791e
Taha Yücegök <77504139+tahayucegokk@users.noreply.github.com>
2023-01-30 01:14:36 +0300

    Create clean.sh
    Clear test files.

---
+++ ft_putstr/clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+files=$(find . -name "*_fail")
+
+rm -rf error!
+rm -rf test.c
+rm -rf a.out
+rm -rf file1
+rm -rf file2
+rm -rf capture_1.txt
+rm -rf capture_2.txt
+
+if [ -n "$files" ]; then
+  for file in $files
+  do
+    rm $file
+    rm -rf *_passed
+    touch fail
+    exit 1
+  done
+else
+  rm -rf *_fail
+  rm -rf *_passed
+  touch passed
+fi
